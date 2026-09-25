e9b488d08ce9eeda5bd87c27c636fa615934b46b
Vladislav <vladislavpaderin@gmail.com>
2026-09-25 15:24:43 +0700

    0.3e-ui: add compact section navigation

--- src/ara/ARAPluginEditor.h
+++ src/ara/ARAPluginEditor.h
@@ -14,16 +14,40 @@ public:
     ~SmartImproviserARAEditor() override = default;
 
     void paint(juce::Graphics& g) override;
-    void resized() override {}
+    void resized() override;
 
 private:
+    enum class Panel
+    {
+        material,
+        sources,
+        harmonic,
+        ara
+    };
+
     void timerCallback() override;
+    void setActivePanel(Panel panel);
+    void refreshPanelView(bool resetScroll);
+    void updatePanelButtons();
 
     SmartImproviserARAProcessor& processor;
     SharedHarmonicContextSnapshot cachedShared;
     smartimproviser::harmony::HarmonicSituation cachedSituation;
-    juce::String improvisationText;
-    juce::TextEditor improvisationDetails;
+
+    Panel activePanel = Panel::material;
+
+    juce::String summaryContext;
+    juce::String summaryMeta;
+    juce::String materialText;
+    juce::String sourcesText;
+    juce::String harmonicText;
+    juce::String araText;
+
+    juce::TextButton materialButton;
+    juce::TextButton sourcesButton;
+    juce::TextButton harmonicButton;
+    juce::TextButton araButton;
+    juce::TextEditor detailsView;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SmartImproviserARAEditor)
 };
